6375cdd0f32fa8873f4b3510d86546db34fdb340
Ajay MT <ajay.tatachar@gmail.com>
2021-06-04 16:16:05 -0500

    build nfa

--- lib/kenfa.ml
+++ lib/kenfa.ml
@@ -14,22 +14,22 @@ type regexp_node = Atom of char
                  | Alternation of regexp_node * regexp_node
                  | Group of regexp_node list
 
+(* let rec show_node node = match node with
+ *   | Atom a -> String.of_char a
+ *   | Plus n | Star n | Once n ->
+ *      let substr = show_node n in
+ *      let op = match node with
+ *        | Plus _ -> "+ " | Star _ -> "* " | _ -> "? "
+ *      in
+ *      "(" ^ op ^ substr ^ ")"
+ *   | Alternation (one, two) ->
+ *      let (a, b) = (show_node one, show_node two) in
+ *      "(" ^ a ^ " | " ^ b ^ ")"
+ *   | Group nodes ->
+ *      "(" ^ (String.concat ~sep:" " @@ List.map ~f:show_node nodes) ^ ")" *)
+
 let ( let* ) x f = Result.bind x ~f:f
 let ( let+ ) x f = Result.map ~f:f x
-
-let rec show_node node = match node with
-  | Atom a -> String.of_char a
-  | Plus n | Star n | Once n ->
-     let substr = show_node n in
-     let op = match node with
-       | Plus _ -> "+ " | Star _ -> "* " | _ -> "? "
-     in
-     "(" ^ op ^ substr ^ ")"
-  | Alternation (one, two) ->
-     let (a, b) = (show_node one, show_node two) in
-     "(" ^ a ^ " | " ^ b ^ ")"
-  | Group nodes ->
-     "(" ^ (String.concat ~sep:" " @@ List.map ~f:show_node nodes) ^ ")"
 
 let parse_regexp re =
   let rec aux re nodes expect_paren = match re with
@@ -67,10 +67,73 @@ let parse_regexp re =
   let+ (nodes, _) = aux re [] false in
   List.rev nodes
 
+type nfa_node = Accept
+              | Continue of int
+              | Split of int * int
+              | Match of char * int
+
+let nfa_of_regexp_nodes nodes =
+  let rec aux state node =
+    let (next_id, nfa_nodes) = state in
+    match node with
+    | Atom a ->
+       let nfa_node = Match (a, next_id + 1) in
+       (next_id + 1, (next_id, nfa_node) :: nfa_nodes)
+    | Group nodes ->
+       let (next_id, nfa_nodes) =
+         List.fold_left nodes ~init:(next_id, nfa_nodes) ~f:aux
+       in
+       (next_id + 1, nfa_nodes)
+    | Alternation (a, b) ->
+       let split_node_id = next_id in
+       let first_branch_id = next_id + 1 in
+       let (first_branch_target, nfa_nodes) = aux (first_branch_id, nfa_nodes) a in
+       let second_branch_id = first_branch_target + 1 in
+       let (second_branch_target, nfa_nodes) =
+         aux (second_branch_id, nfa_nodes) b
+       in
+       let continue_node_pair = (first_branch_target, Continue second_branch_target) in
+       let split_node_pair =
+         (split_node_id, Split (first_branch_id, second_branch_id))
+       in
+       (second_branch_target + 1,
+        split_node_pair :: continue_node_pair :: nfa_nodes)
+    | Once n ->
+       let split_node_id = next_id in
+       let match_branch_id = next_id + 1 in
+       let (match_target, nfa_nodes) = aux (match_branch_id, nfa_nodes) n in
+       let split_node_pair = (split_node_id, Split (match_target, match_branch_id)) in
+       (match_target + 1, split_node_pair :: nfa_nodes)
+    | Star n ->
+       let split_node_id = next_id in
+       let match_branch_id = next_id + 1 in
+       let (match_target, nfa_nodes) = aux (match_branch_id, nfa_nodes) n in
+       let second_split_node_pair =
+         (match_target, Split (match_branch_id, match_target + 1))
+       in
+       let split_node_pair =
+         (split_node_id, Split (match_branch_id, match_target + 1))
+       in
+       (match_target + 1, split_node_pair :: second_split_node_pair :: nfa_nodes)
+    | Plus n ->
+       let (next_id, nfa_nodes) = aux (next_id, nfa_nodes) n in
+       aux (next_id, nfa_nodes) (Star n)
+  in
+  let (final_target, nfa_nodes) = List.fold_left nodes ~init:(0, []) ~f:aux in
+  (final_target, Accept) :: nfa_nodes
+
 let compile_regexp re =
-  match parse_regexp (String.to_list re) with
-  | Ok nodes -> Error (String.concat ~sep:" " @@ List.map ~f:show_node nodes)
-  | Error e -> Error e
+  let show_nfa_node (id, variant) =
+    let vs = match variant with
+      | Accept -> "A" | Continue i -> "C " ^ (Int.to_string i)
+      | Split (a, b) -> "S " ^ (Int.to_string a) ^ " " ^ (Int.to_string b)
+      | Match (c, i) -> "M " ^ (Char.to_string c) ^ " " ^ (Int.to_string i)
+    in
+    "(" ^ (Int.to_string id) ^ ", " ^ vs ^ ")"
+  in
+  let+ regexp_nodes = parse_regexp (String.to_list re) in
+  let nfa_nodes = nfa_of_regexp_nodes regexp_nodes in
+  String.concat ~sep:" " @@ List.map nfa_nodes ~f:show_nfa_node
 
 let string_match fsm s =
   let (graph, accepted) = fsm in
@@ -82,5 +145,5 @@ let string_match fsm s =
   in
   let final_state = String.fold_result s ~init:0 ~f:transition in
   match final_state with
-  | Ok final_state -> List.mem accepted final_state ~equal:phys_equal
+  | Ok final_state -> List.mem accepted final_state ~equal:(=)
   | Error () -> false
