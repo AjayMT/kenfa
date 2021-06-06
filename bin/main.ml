abdd383d1b26cf7aa3d5fdc531657658092bf7a5
Ajay MT <ajay.tatachar@gmail.com>
2021-06-06 18:11:56 -0500

    init CLI

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+
+open Base
+open Kenfa
+
+let () =
+  let regexp_string = Array.get (Sys.get_argv ()) 1 in
+  match compile_regexp regexp_string with
+  | Error e -> Stdlib.prerr_endline e
+  | Ok dfa -> Stdlib.print_endline @@ draw_dfa dfa
