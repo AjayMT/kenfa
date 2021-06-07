6e25fb3c72de6fa5ee8ca1ac4d8a4421da2f246b
Ajay MT <ajay.tatachar@gmail.com>
2021-06-06 22:21:51 -0500

    cli

--- bin/main.ml
+++ bin/main.ml
@@ -3,7 +3,23 @@ open Base
 open Kenfa
 
 let () =
-  let regexp_string = Array.get (Sys.get_argv ()) 1 in
-  match compile_regexp regexp_string with
-  | Error e -> Stdlib.prerr_endline e
-  | Ok dfa -> Stdlib.print_endline @@ draw_dfa dfa
+  let usage_msg =
+    "kenfa <regexp> <input>\nkenfa -d <regexp> [<input>]\n\nOptions:"
+  in
+  let args = ref [] in
+  let draw_flag = ref false in
+  let options = [
+      ("-d", Caml.Arg.Set draw_flag, "\tOutput the generated DFA as a DOT diagram.")
+    ] in
+  let () = Caml.Arg.parse options (fun a -> args := a :: !args) usage_msg in
+  match (List.rev !args) with
+  | [] -> Caml.Arg.usage options usage_msg
+  | regexp :: rest ->
+     match compile_regexp regexp with
+     | Error e -> Stdlib.prerr_endline e
+     | Ok dfa ->
+        if !draw_flag then Stdlib.print_endline @@ draw_dfa dfa else ();
+        match rest with
+        | input :: _ -> Stdlib.print_endline @@ Bool.to_string @@
+                          string_match dfa input
+        | [] -> Caml.Arg.usage options usage_msg
--- lib/kenfa.mli
+++ lib/kenfa.mli
@@ -8,5 +8,5 @@ val compile_regexp : string -> (dfa, string) result
 (** [string_match d s] checks whether the string [s] matches the DFA [d] exactly. *)
 val string_match : dfa -> string -> bool
 
-(** [draw_dfa d] outputs a DOT diagram that represents [d] *)
+(** [draw_dfa d] outputs a DOT diagram that represents the DFA [d]. *)
 val draw_dfa : dfa -> string
